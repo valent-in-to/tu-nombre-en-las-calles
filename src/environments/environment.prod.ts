ffbb3c9bc18d803912a2ec1dbb513db3c72e11bf
valent-in-to <47725605+valent-in-to@users.noreply.github.com>
2020-08-30 16:17:53 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   streetsAPI: 'https://apis.datos.gob.ar/georef/api/calles',
   chartsAPI: 'http://chart.apis.google.com/chart',
   geoAPI: 'http://valentinto.pythonanywhere.com/localidades',
-  mapBoxToken: 'pk.eyJ1IjoidmFsZW50LWluLXRvIiwiYSI6ImNrZG93djh2bTA4M2wyd2p6MGtvODZjMTAifQ.NVjtvUlMkAynr9j81vDYPw'
+  mapBoxToken: ' '
 };
